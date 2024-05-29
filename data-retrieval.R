4e2161c9925a828760f79d712446af2526ba9891
Lera Kantsypa <kantsypa.valeriia@gmail.com>
2024-05-29 09:38:55 +0300

    update to match

--- data-retrieval.R
+++ data-retrieval.R
@@ -14,13 +14,14 @@ tse <- sampleMetadata |>
   filter(body_site == 'stool') |>
   #filter(pregnant != 'yes') |>
   #filter(lactating != 'yes') |>
-  returnSamples("relative_abundance", rownames = "short")
+  returnSamples("relative_abundance", rownames = "NCBI")
 
 # obtain the data as dfs
 counts <- as.data.frame(assays(tse)[[1]])
 meta <- data.frame(colData(tse))
 
 # ====== Balance the dataset ======
+# ommitted the step to retrieve all the data for two studies
 # Find the counts per study name
 study_counts <- table(meta$study_name)
 
@@ -28,19 +29,19 @@ study_counts <- table(meta$study_name)
 ## either select the minimum of study counts
 #min_count <- min(study_counts)
 ## or a constant
-min_count <- 10
+#min_count <- 10
 
 # Randomly sample that number of samples from each study name
-sampled_data <- lapply(names(study_counts), function(study) {
-  subset(meta, study_name == study)[sample(1:nrow(subset(meta, study_name == study)), min_count), ]
-})
+#sampled_data <- lapply(names(study_counts), function(study) {
+#  subset(meta, study_name == study)[sample(1:nrow(subset(meta, study_name == study)), min_count), ]
+#})
 
 # Combine sampled datasets into a single balanced dataset
-balanced_meta <- do.call(rbind, sampled_data)
-table(balanced_meta$study_name)
+#balanced_meta <- do.call(rbind, sampled_data)
+#table(balanced_meta$study_name)
 
 # filter the counts
-counts <- counts[, rownames(balanced_meta)]
+#counts <- counts[, rownames(balanced_meta)]
 
 # to check the number of unique values in each column
 #sapply(meta, function(x) length(unique(x)))
@@ -52,5 +53,10 @@ table2 <- meta %>%
   group_by('Назва дослідження' = study_name) %>% 
   summarise('PMID' = first(PMID),
             'Платформа секвенування' = toString(unique(sequencing_platform)),
-            'Набор для екстракції ДНК' = toString(unique(DNA_extraction_kit)))
+            'Набор для екстракції ДНК' = toString(unique(DNA_extraction_kit)),
+            'Здорових зразків' = sum(disease == 'healthy'),
+            'Ракових зразків' = sum(disease == 'CRC'))
+
+
+
 write.csv(table2, "camp_normalization_data-retrieval/table2.csv")
